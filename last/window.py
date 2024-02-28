410324be1f5c16d38028a9423d264be745195f57
tuan220199 <phamtuan.220199@gmail.com>
2024-02-28 10:39:41 +0200

    Add comment for last

--- last/window.py
+++ last/window.py
@@ -7,7 +7,40 @@ from evaluationwindow import EvaluationWindow
 from gforce import GForceProfile
 
 class Window(QtWidgets.QMainWindow):
+    """
+    Class representing the main window of the application.
+
+    This window manages a stacked widget for switching between different pages.
+    Each page is registered and stored in a dictionary for easy access.
+
+    Attributes:
+        stacked_widget (QtWidgets.QStackedWidget): Stacked widget for managing pages.
+        m_pages (dict): Dictionary to store registered pages.
+        GF (GForceProfile): Instance of GForceProfile for data management.
+
+    Signals:
+        gotoSignal (str): Signal emitted when switching between pages.
+
+    Methods:
+        __init__(self, parent=None):
+            Initializes the main window.
+
+        register(self, widget, name):
+            Registers a page widget with a given name.
+
+        addData_callbackFunc(self, value):
+            Callback function to add data to the figure.
+
+        goto(self, name):
+            Navigates to the specified page.
+    """
     def __init__(self, parent=None):
+        """
+        Initializes the main window.
+
+        Args:
+            parent: Parent widget (default: None).
+        """
         super().__init__(parent)
         self.GF = GForceProfile()
          
@@ -23,15 +56,34 @@ class Window(QtWidgets.QMainWindow):
         self.goto("main0")
 
     def register(self, widget, name):
+        """
+        Registers a page widget with a given name.
+
+        Args:
+            widget: Page widget to be registered.
+            name (str): Name of the page.
+        """
         self.m_pages[name] = widget
         self.stacked_widget.addWidget(widget)
         if isinstance(widget, PageWindow):
             widget.gotoSignal.connect(self.goto)
     def addData_callbackFunc(self, value):
+        """
+        Callback function to add data to the figure.
+
+        Args:
+            value: Data value to be added.
+        """
         self.myFig.addData(value)
 
     @QtCore.pyqtSlot(str)
     def goto(self, name):
+        """
+        Navigates to the specified page.
+
+        Args:
+            name (str): Name of the page to navigate to.
+        """
         comeback = int(name[-1])
         name = name[:-1]
         if name in self.m_pages:
